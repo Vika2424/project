5e55fc00d489105ce839cab135fe989c9bc54b1a
Vika <vika9282@ukr.net>
2022-01-07 20:24:03 +0200

    triggers optimization

--- src/lwc/userForm/userForm.js
+++ src/lwc/userForm/userForm.js
@@ -6,16 +6,16 @@ import {LightningElement} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import USER_ID from '@salesforce/user/Id';
 import COMPANY_NAME_FIELD from '@salesforce/schema/User.CompanyName';
-import NICKNAME_FIELD from '@salesforce/schema/User.CommunityNickname'
+import FIRST_NAME_FIELD from '@salesforce/schema/User.FirstName';
+import LAST_NAME_FIELD from '@salesforce/schema/User.LastName';
 import TITLE_FIELD from '@salesforce/schema/User.Title';
 import PHONE_FIELD from '@salesforce/schema/User.Phone';
-import COUNTRY_FIELD from '@salesforce/schema/User.Country';
 import CITY_FIELD from '@salesforce/schema/User.City';
 
 export default class UserForm extends LightningElement {
     userId = USER_ID;
 
-    fields = [NICKNAME_FIELD, COMPANY_NAME_FIELD, TITLE_FIELD, PHONE_FIELD, COUNTRY_FIELD, CITY_FIELD];
+    fields = [FIRST_NAME_FIELD, LAST_NAME_FIELD, COMPANY_NAME_FIELD, TITLE_FIELD, PHONE_FIELD, CITY_FIELD];
     handleSuccess() {
         const toastEvent = new ShowToastEvent({
             title: "User information successfully updated",
